e898ad4ffdedcabb5138a7b29e28f290c89e965b
Kamlesh Bhatt <kb55262555@gmail.com>
2023-04-19 23:42:11 +0530

    Updated Home Page

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 //For Slider
-let count = 1;
+var count = 1;
 
 setInterval(function () {
     document.getElementById('radio' + count).checked = true;
@@ -8,3 +8,32 @@ setInterval(function () {
         count = 1;
     }
 }, 3000);
+var slideLeft = 1;
+var slideRight = 4;
+function slideMoveLeft() {
+    if (slideLeft >= 2) {
+        document.getElementById("sc" + slideRight).style.display = "none";
+        slideLeft -= 1;
+        slideRight -= 1;
+
+        for (var i = slideLeft; i <= slideRight; i++) {
+            document.getElementById("sc" + i).style.display = "inline-block";
+        }
+    } else {
+        return;
+    }
+}
+
+function slideMoveRight() {
+    if (slideRight <= 11) {
+        document.getElementById("sc" + slideLeft).style.display = "none";
+        slideLeft++;
+        slideRight++;
+
+        for (var i = slideLeft; i <= slideRight; i++) {
+            document.getElementById("sc" + i).style.display = "inline-block";
+        }
+    } else {
+        return;
+    }
+}
