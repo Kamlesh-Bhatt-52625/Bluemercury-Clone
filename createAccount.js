1a8616f408a463202c07ac271a84406c61b15943
Anwar-3108 <syed.sahab9628@gmail.com>
2023-04-19 20:32:02 +0530

    signin and log in authentication

---
+++ createAccount.js
@@ -0,0 +1,28 @@
+document.querySelector("form").addEventListener("submit", createAccount);
+
+
+let credentialArr =JSON.parse(localStorage.getItem("userCred")) || [];
+
+function createAccount() {
+    event.preventDefault();
+
+    let email = document.querySelector("#email").value;
+    let fname = document.querySelector("#fname").value;
+    let lname = document.querySelector("#lname").value;
+    let pass = document.querySelector("#pass").value;
+
+
+    let dataObj = {
+        email: email,
+        fname: fname,
+        lname: lname,
+        pass: pass,
+
+    }
+
+    credentialArr.push(dataObj);
+    console.log(credentialArr);
+
+    localStorage.setItem("userCred", JSON.stringify(credentialArr))
+
+}
