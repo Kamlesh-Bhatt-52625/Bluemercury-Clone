610661881b5332133b9076b1390a1f6e3e4bbbab
Developer Awdhesh <awdheshthinker@gmail.com>
2023-04-19 10:27:26 +0530

    responsive navbar done

---
+++ navbar.js
@@ -0,0 +1,8 @@
+document.getElementById("nav").addEventListener("click", toggleFun);
+
+    function toggleFun(){
+
+       let btn =  document.querySelector("ul");
+       btn.classList.toggle("bt")
+
+    }
