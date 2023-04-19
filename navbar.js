15818c5c6a0e7dd3deca86b86f6b1644577f770a
Developer Awdhesh <awdheshthinker@gmail.com>
2023-04-19 10:43:45 +0530

    small changes

--- navbar.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("nav").addEventListener("click", toggleFun);
-
-    function toggleFun(){
-
-       let btn =  document.querySelector("ul");
-       btn.classList.toggle("bt")
-
-    }
