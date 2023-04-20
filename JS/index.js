01f4d8fe536697b3de6a94d7ea60ea78ad46cb44
Kamlesh Bhatt <kb55262555@gmail.com>
2023-04-20 09:18:15 +0530

    Added new Slide Bar and some other things in the home page

--- JS/index.js
+++ JS/index.js
@@ -10,11 +10,39 @@ setInterval(function () {
 }, 3000);
 var slideLeft = 1;
 var slideRight = 4;
+
+var slideLeft1 = 1;
+var slideRight1 = 4;
+
+function slideShow() {
+    for (let i = slideLeft; i <= slideRight; i++) {
+        document.getElementById("sc" + i).style.display = "inline-block";
+    }
+    for (let i = slideLeft; i <= slideRight; i++) {
+        document.getElementById("scc" + i).style.display = "inline-block";
+    }
+}
+
+
+function slideMoveRight() {
+    if (slideRight <= 11) {
+        document.getElementById("sc" + slideLeft).style.display = "none";
+        slideLeft++;
+        slideRight++;
+        
+        for (var i = slideLeft; i <= slideRight; i++) {
+            document.getElementById("sc" + i).style.display = "inline-block";
+        }
+    } else {
+        return;
+    }
+}
+
 function slideMoveLeft() {
     if (slideLeft >= 2) {
         document.getElementById("sc" + slideRight).style.display = "none";
-        slideLeft -= 1;
-        slideRight -= 1;
+        slideLeft--;
+        slideRight--;
 
         for (var i = slideLeft; i <= slideRight; i++) {
             document.getElementById("sc" + i).style.display = "inline-block";
@@ -24,14 +52,28 @@ function slideMoveLeft() {
     }
 }
 
-function slideMoveRight() {
-    if (slideRight <= 11) {
-        document.getElementById("sc" + slideLeft).style.display = "none";
-        slideLeft++;
-        slideRight++;
+function slideMoveRight1() {
+    if (slideRight1 <= 11) {
+        document.getElementById("scc" + slideLeft1).style.display = "none";
+        slideLeft1++;
+        slideRight1++;
 
-        for (var i = slideLeft; i <= slideRight; i++) {
-            document.getElementById("sc" + i).style.display = "inline-block";
+        for (let i = slideLeft1; i <= slideRight1; i++) {
+            document.getElementById("scc" + i).style.display = "inline-block";
+        }
+    } else {
+        return;
+    }
+}
+
+function slideMoveLeft1() {
+    if (slideLeft1 >= 2) {
+        document.getElementById("scc" + slideRight1).style.display = "none";
+        slideLeft1--;
+        slideRight1--;
+
+        for (let i = slideLeft1; i <= slideRight1; i++) {
+            document.getElementById("scc" + i).style.display = "inline-block";
         }
     } else {
         return;
