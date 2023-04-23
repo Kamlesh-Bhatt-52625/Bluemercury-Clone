4ce7795ad40a6cb3bd53f6e63b7589e8ae02db30
Anwar-3108 <syed.sahab9628@gmail.com>
2023-04-24 01:03:52 +0530

    added Cart shop , cart , wishlist , empty pages

---
+++ navigationpages/shopPage/shop.js
@@ -0,0 +1,473 @@
+let  productData = [
+    {id: 1,
+        image_url :"./Assets/5.jpg",
+        name: "Dr. Barbara Sturm",
+        about:"Super Anti-Aging Body Serum ",
+      price: "1170",
+    
+    },
+    {id: 2,
+        image_url :"./Assets/6.jpg",
+        name: "Dr. Barbara Sturm",
+        about:"Super Anti-Aging Body Serum ",
+
+      price: 699,
+     
+    },
+    {id: 3,
+        image_url :"./Assets/7.jpg",
+        name: "Dr. Barbara Sturm",
+        about:"Super Anti-Aging Body Serum ",
+
+      price: 945,
+   
+    },
+    {id: 4,
+        image_url :"./Assets/8.jpg",
+        name: "Dr. Barbara Sturm",
+        about:"Super Anti-Aging Body Serum ",
+
+      price: 399,
+     
+    },
+    {id: 5,
+        image_url :"./Assets/9.jpg",
+        name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1620,
+
+    },
+    {id: 6,
+        image_url :"./Assets/10.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1440,
+    },
+    {id: 7,
+        image_url :"./Assets/11.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1999,
+    },
+    {id: 8,
+        image_url :"./Assets/12.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 769,
+    },
+    {id: 9,
+        image_url :"./Assets/13.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1299,
+    },
+    {id: 10,
+        image_url :"./Assets/14.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 699,
+    },
+    {id: 11,
+        image_url :"./Assets/15.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1620,
+    },
+    {id: 12,
+      image_url :"./Assets/16.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      
+      price: 1620,
+    },
+    {id: 13,
+      image_url :"./Assets/17.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 599,
+    },
+    {id: 14,
+      image_url :"./Assets/18.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1575,
+    },
+    {id: 15,
+      image_url :"./Assets/19.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 9999,
+    },
+    {id: 16,
+      image_url :"./Assets/20.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 779,
+    },
+    {id: 17,
+      image_url :"./Assets/21.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+      price: 1019,
+    },
+    {id: 18,
+      image_url :"./Assets/22.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1019,
+    },
+    {id: 19,
+      image_url :"./Assets/23.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1575,
+    },
+    {id: 20,
+      image_url :"./Assets/24.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 3499,
+    },
+    {id: 21,
+      image_url :"./Assets/25.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 749,
+    },
+    {id: 22,
+      image_url :"./Assets/26.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1149,
+    },
+    {id: 23,
+      image_url :"./Assets/27.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1620,
+    },
+    {id: 24,
+      image_url :"./Assets/28.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 779,
+    },
+    {id: 25,
+      image_url :"./Assets/29.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 3499,
+    },
+    {id: 26,
+      image_url :"./Assets/30.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 3499,
+    },
+    {id: 27,
+      image_url :"./Assets/31.jpg",
+      name: "Vintner's Daughter",
+      about:"Active Renewal Cleanser",
+      description:"A gently foaming cream formula, combining a double cleanse into a single illuminating step, visibly brightening, balancing and energizing skin.",
+      price: 1399,
+    },
+    {id: 28,
+      image_url :"./Assets/31.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1575,
+    },
+    {id: 29,
+      image_url :"./Assets/32.jpg",
+      name: "Indie Lee",
+      about:"AHA Exfoliating Toner",
+      description:"A refreshing, exfoliating 15% blend of Lactic Acid, Glycolic Acid and Prickly Pear complex that leaves skin soft, smooth and glowing.",
+      price: 3919,
+    },
+    {id: 30,
+      image_url :"./Assets/33.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1620,
+    },
+    {id: 31,
+      image_url :"./Assets/34.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1620,
+    },
+    {id: 32,
+      image_url :"./Assets/35.jpg",
+      name: "M-61",
+      about:"Hydraboost Body Sunscreen SPF 30",
+      description:"Broad spectrum SPF 30 hydrating and firming algae extract and peptide body sunscreen",
+
+      price: 7599,
+    },
+    {id: 33,
+      image_url :"./Assets/36.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 909,
+    },
+    {id: 34,
+      image_url :"./Assets/37.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 779,
+    },
+    {id: 35,
+      image_url :"./Assets/38.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1749,
+    },
+    {id: 36,
+      image_url :"./Assets/39.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 37,
+      image_url :"./Assets/40.jpg",
+      name: "Chantecaille",
+      about:"Wild Meadows Eye Quartet (Limited Edition)",
+
+      description:"This elegant eye quad includes two cream-infused pearlescent shimmering shades, one satiny-soft matte shade, and one luminous satin hue",
+      price: 1079,
+    },
+    {id: 38,
+      image_url :"./Assets/41.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 39,
+      image_url :"./Assets/53.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 40,
+      image_url :"./Assets/54.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 41,
+      image_url :"./Assets/55.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 42,
+      image_url :"./Assets/56.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 43,
+      image_url :"./Assets/56.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    {id: 44,
+      image_url :"./Assets/58.jpg",
+      name: "Dr. Barbara Sturm",
+      about:"Super Anti-Aging Body Serum ",
+
+      price: 1079,
+    },
+    // {id: 45,
+    //   image_url :"./Assets/59.jpg",
+    //   name: "Dr. Barbara Sturm",
+    //   about:"Super Anti-Aging Body Serum ",
+    //   price: 1079,
+    // },
+  ];
+
+
+
+
+
+
+  productData.map(function(e,i){
+    let pro =document.createElement("div") ;
+    pro.setAttribute("class" ,"pro") ;
+
+   
+    let image=document.createElement("img") ;
+    image.setAttribute("src" ,e.image_url)
+    image.setAttribute("alt" ,e.name);
+    
+    // image.addEventListener("click" ,function(){
+    //   showDetails(e) ;
+    // })
+
+
+
+    let des =document.createElement("div") ;
+    des.setAttribute("class" ,"des") ;
+
+    let span =document.createElement("span") ;
+    span.textContent=e.name ;
+    let h5 =document.createElement("h5") ;
+    h5.textContent=e.about ;
+
+
+    let star =document.createElement("div");
+    star.setAttribute("class" ,"star") ;
+
+
+    let i1 =document.createElement("i");
+    i1.setAttribute("class" ,"fa-solid fa-star") ;
+
+    let i2 =document.createElement("i");
+    i2.setAttribute("class" ,"fa-solid fa-star") ;
+
+    let i3 =document.createElement("i");
+    i3.setAttribute("class" ,"fa-solid fa-star") ;
+    
+    let i4 =document.createElement("i");
+    i4.setAttribute("class" ,"fa-solid fa-star") ;
+
+
+    let h4 =document.createElement("h4") ;
+    h4.textContent=e.price;
+
+    let a =document.createElement("a") ;
+    a.setAttribute("href" ,"#") ;
+ 
+    let i5 =document.createElement("i") ;
+    i5.setAttribute("class" ,"fa-sharp fa-solid fa-cart-shopping cart")  ;
+
+    i5.addEventListener("click" ,function(){
+      addToCart(e) ;
+  })
+
+
+    star.append(i1,i2,i3,i4) ;
+
+    des.append(span ,h5 ,star ,h4) ;
+    a.append(i5) ;
+    pro.append(image ,des ,a ) ;
+
+    document.querySelector(".pro-container").append(pro) ;
+
+  })
+
+  let cartArr=JSON.parse(localStorage.getItem("cartItem"))||[] ;
+
+    function addToCart(product){
+console.log(product);
+cartArr.push(product)
+localStorage.setItem("cartItem" ,JSON.stringify(cartArr))
+    }
+
+
+    // function showDetails(product){
+    //   document.querySelector(".pro").style="none" ;
+    // }
+
+
+
+    
+// let featuredProducts=[ 
+//   {id: 1,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 2,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 3,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 4,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 5,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 6,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 7,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 8,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 9,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+//   {id: 10,
+//   image_url :"./Assets/5.jpg",
+//   name: "Dr. Barbara Sturm",
+//   about:"Super Anti-Aging Body Serum ",
+// price: "1170",
+
+// },
+
+
+// ];
+
+
