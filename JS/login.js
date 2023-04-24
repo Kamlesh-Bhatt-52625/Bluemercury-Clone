7295d438dc35d8e4339d89fd08c4c1ca5c0a581d
Kamlesh Bhatt <kb55262555@gmail.com>
2023-04-24 16:29:55 +0530

    Merged more than 90%

---
+++ JS/login.js
@@ -0,0 +1,24 @@
+document.querySelector("form").addEventListener("submit" ,login) ;
+
+let getCredDetails =JSON.parse(localStorage.getItem("userCred"))||[] ;
+
+function login(){
+    event.preventDefault() ;
+    let email =document.querySelector("#email").value ;
+    let pass =document.querySelector("#pass").value ;
+    getCredDetails.map(function(e,i){
+
+        if(email==e.email && pass == e.pass){
+            // alert("hello user")
+           
+            window.location.href="./payment.html"
+        }else{
+            alert("wrong cred")
+            window.location.href="./login.html"
+
+        }
+    })
+
+
+
+}
